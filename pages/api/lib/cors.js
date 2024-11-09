ae4dc958ab8015964e5951395c026d397c0fd07c
Rahul Gotharwal <rahulgotharwal877@gmail.com>
2024-11-09 16:40:09 +0530

    again change the url of cors origin

--- pages/api/lib/cors.js
+++ pages/api/lib/cors.js
@@ -4,7 +4,7 @@ import Cors from 'cors';
 const cors = Cors({
   //https://ecommerce-3vsnqylwx-rahulgotharwals-projects.vercel.app/
   // we need to write this frontend url in the cors origin insted the => 'https://ecommerce-lake-xi.vercel.app
-  origin: 'https://ecommerce-3vsnqylwx-rahulgotharwals-projects.vercel.app', // Update with your new frontend URL
+  origin: 'https://ecommerce-rahulgotharwals-projects.vercel.app', // Update with your new frontend URL
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
 });
