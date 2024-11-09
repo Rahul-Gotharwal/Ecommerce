70867bf42fc5049f2601f011e0bbf79b17c096cf
Rahul Gotharwal <rahulgotharwal877@gmail.com>
2024-11-09 16:03:05 +0530

    next.cionfig.js file is changed

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  env: {
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL, // or any other environment variable
+  },
+};
 
-module.exports = nextConfig 
+module.exports = nextConfig;
